3e89e429d13426d22194d3fc51b16b9ab432db97
Roland <rbayor16@gmail.com>
2024-05-20 06:39:09 +0000

    initial setup

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'RB Developer Logs' as const;
+export const description = 'RB Developer Logs' as const;
+export const url = dev ? 'http://localhost:5173/' : '';
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<div class="dark flex min-h-screen w-full flex-col items-center p-3">
+	<div class="flex w-full max-w-3xl flex-grow flex-col items-center justify-center">
+		<nav class="flex w-full flex-row items-center justify-between">
+			<h1>Dev Logs</h1>
+			<img src="/me.webp" alt="RB Dev" height="45" width="45" class="rounded-full" />
+		</nav>
+
+		<main class="flex-grow">heelo</main>
+
+		<footer class="w-full">
+			<p>Copyright &copy; {new Date().getFullYear()} Roland Bayor</p>
+		</footer>
+	</div>
+</div>
