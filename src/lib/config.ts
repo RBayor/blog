852d2b112f83d3e8867e99392fa3a255bed4ae9f
Roland <rbayor16@gmail.com>
2024-05-22 00:31:43 +0000

    remove unused

--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from '$app/environment';
-
-export const title = 'RB Developer Logs' as const;
-export const description = 'RB Developer Logs' as const;
-export const url = dev ? 'http://localhost:5173/' : '';
