4c8cc83b5e1f4c2692c83dc6ef1bf4cd4df5101c
Roland <rbayor16@gmail.com>
2024-05-22 00:40:10 +0000

    added post page

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { formatDate } from "@/utils";
+	const { data } = $props();
+</script>
+
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta property="og:tile" content={data.meta.title} />
+	<meta property="og:type" content="article" />
+</svelte:head>
+
+<article class="flex w-full flex-grow flex-col">
+	<hgroup>
+		<h1 class="text-2xl font-bold tracking-tight md:text-3xl">{data.meta.title}</h1>
+		<p class="text-sm text-muted-foreground">Published on {formatDate(data.meta.date)}</p>
+	</hgroup>
+	<div class="flex flex-row flex-wrap gap-2">
+		{#each data.meta.tags as tag}
+			<span class="rounded bg-slate-800 px-2 pb-1 text-sm text-muted-foreground">{tag}</span>
+		{/each}
+	</div>
+
+	<div class="h-10"></div>
+
+	<svelte:component this={data.content} />
+</article>
