bd77ab1a65d9a81bfc0615f400b51ea8bfce50cf
Roland <rbayor16@gmail.com>
2024-05-22 00:34:13 +0000

    setup shiki

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,36 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import { escapeSvelte, mdsvex } from "mdsvex";
+import { getHighlighter } from "shiki";
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexConfig = {
+	extensions: [".md"],
+	highlight: {
+		highlighter: async (code, lang = "text") => {
+			const highlighter = await getHighlighter({
+				themes: ["material-theme-darker"],
+				langs: ["typescript"]
+			});
+
+			const html = escapeSvelte(
+				highlighter.codeToHtml(code, {
+					theme: "material-theme-darker",
+					lang
+				})
+			);
+
+			return `{@html \`${html}\`}`;
+		}
+	}
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	extensions: [".svelte", ".md"],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
 			"@/*": "./src/lib"
