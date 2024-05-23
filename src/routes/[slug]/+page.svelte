353491da2eb19556235b9b5c749ddf7b844f64a5
Roland <rbayor16@gmail.com>
2024-05-23 13:01:37 +0000

    improve tags layout
    Signed-off-by: Roland <rbayor16@gmail.com>

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -14,9 +14,9 @@
 		<h1 class="text-2xl font-bold tracking-tight md:text-3xl">{data.meta.title}</h1>
 		<p class="text-sm text-muted-foreground">Published on {formatDate(data.meta.date)}</p>
 	</hgroup>
-	<div class="flex flex-row flex-wrap gap-2">
+	<div class="flex flex-row flex-wrap gap-2 pt-2">
 		{#each data.meta.tags as tag}
-			<span class="rounded bg-slate-800 px-2 pb-1 text-sm text-muted-foreground">{tag}</span>
+			<div class="rounded-lg bg-slate-800 px-2 text-sm text-muted-foreground shadow-lg">@{tag}</div>
 		{/each}
 	</div>
 
