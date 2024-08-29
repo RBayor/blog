aad7cd0296c5d07291e33aa28e5a47faa95ecaa8
Roland <rbayor16@gmail.com>
2024-08-29 06:56:57 +0000

    change to bun

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
 	import { formatDate } from "@/utils";
-	const { meta, content } = $props();
+	const { data } = $props();
+
 </script>
 
 <svelte:head>
-	<title>{meta.title}</title>
-	<meta property="og:tile" content={meta.title} />
+	<title>{data.meta.title}</title>
+	<meta property="og:tile" content={data.meta.title} />
 	<meta property="og:type" content="article" />
 </svelte:head>
 
 <article class="flex w-full flex-grow flex-col">
 	<hgroup>
-		<h1 class="text-2xl font-bold tracking-tight md:text-3xl">{meta.title}</h1>
-		<p class="text-sm text-muted-foreground">Published on {formatDate(meta.date)}</p>
+		<h1 class="text-2xl font-bold tracking-tight md:text-3xl">{data.meta.title}</h1>
+		<p class="text-sm text-muted-foreground">Published on {formatDate(data.meta.date)}</p>
 	</hgroup>
 	<div class="flex flex-row flex-wrap gap-2 pt-2">
-		{#each meta.tags as tag}
+		{#each data.meta.tags as tag}
 			<div class="rounded-lg bg-slate-800 px-2 text-sm text-muted-foreground shadow-lg">@{tag}</div>
 		{/each}
 	</div>
 
 	<div class="h-5"></div>
 
-	{@render content()}
+	{@render data.content()}
 </article>
